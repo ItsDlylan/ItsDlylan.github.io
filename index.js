f5bae243e781d26e6f8acb7509b9dffa56305e23
Dylan Schwindt <dfieldmark69@gmail.com>
2021-09-09 22:22:41 -0700

    Testing email on live githubpage

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
+import emailjs from 'emailjs-com';
+
 // ---
-const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
-const smallMenu = document.querySelector('.header__sm-menu')
+const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont');
+const smallMenu = document.querySelector('.header__sm-menu');
 
 hamMenuBtn.addEventListener('click', () => {
-  if (smallMenu.classList.contains('header__sm-menu--active')) {
-    smallMenu.classList.remove('header__sm-menu--active')
-  } else {
-    smallMenu.classList.add('header__sm-menu--active')
-  }
-})
+	if (smallMenu.classList.contains('header__sm-menu--active')) {
+		smallMenu.classList.remove('header__sm-menu--active');
+	} else {
+		smallMenu.classList.add('header__sm-menu--active');
+	}
+});
 
 // ---
-const headerLogoConatiner = document.querySelector('.header__logo-container')
+const headerLogoConatiner = document.querySelector('.header__logo-container');
 
 headerLogoConatiner.addEventListener('click', () => {
-  location.href = 'index.html'
-})
+	location.href = 'index.html';
+});
+const sendEmail = (e) => {
+	e.preventDefault();
+
+	emailjs
+		.sendForm(
+			'gmail',
+			'aebf7c8be72205c3b67521842b2b2021',
+			e.target,
+			'user_SCXmKosxT5hSDF3lbDIFb',
+		)
+		.then(
+			(result) => {
+				console.log(result.text);
+			},
+			(error) => {
+				console.log(error.text);
+			},
+		);
+};
+
+const form = document.querySelector('.contact__form');
+form.addEventListener('submit', sendEmail);
