26914f67f9694ac73fbb6a15a261043e4b957826
Dylan Schwindt <dfieldmark69@gmail.com>
2021-09-09 23:37:34 -0700

    Email and BudgetChecker Completed Portfolio V1.0 is now done.

--- sendEmail.js
+++ sendEmail.js
@@ -3,23 +3,43 @@ const sendEmail = (e) => {
 	console.log('hit');
 
 	const text = document.createElement('h1');
+	const modalSection = document.querySelector('.form__message');
+	const formInputName = document.querySelector('#name');
+	const formInputEmail = document.querySelector('#email');
+	const formInputSubject = document.querySelector('#subject');
+	const formInputMessage = document.querySelector('#message');
+
 	emailjs.sendForm('service_begjst4', 'template_iads0pq', e.target).then(
 		function (response) {
 			console.log('SUCCESS!', response.status, response.text);
+			modalSection.style.marginTop = '5rem';
 			text.textContent = 'Email Sent! Thank you!';
 			document.querySelector('.form__message').appendChild(text);
 			text.classList.add('form__message-h1');
 			setTimeout(() => {
 				document.querySelector('.form__message-h1').remove();
+
+				// reset form values to empty strings
+				formInputName.value = '';
+				formInputEmail.value = '';
+				formInputMessage.value = '';
+				formInputSubject.value = '';
 			}, 1000);
 		},
 		function (err) {
 			console.log('FAILED...', err);
 			text.textContent = `Something went wrong, Please try again later: ${err} `;
-			document.querySelector('.form__message').appendChild(text);
+			modalSection.style.marginTop = '5rem';
+			modalSection.appendChild(text);
 			text.classList.add('form__message-h1');
 			setTimeout(() => {
 				document.querySelector('.form__message-h1').remove();
+
+				// reset form values to empty strings
+				formInputName.value = '';
+				formInputEmail.value = '';
+				formInputMessage.value = '';
+				formInputSubject.value = '';
 			}, 1000);
 		},
 	);
